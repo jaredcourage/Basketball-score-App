aa6abaa158fadff3af8757bc533d75106d239ef1
jaredcourage <145121823+jaredcourage@users.noreply.github.com>
2024-11-06 08:37:39 +0100

    commit to main

---
+++ basket.js
@@ -0,0 +1,118 @@
+
+let homeEl = document.getElementById('home-el');
+let guestEl = document.getElementById('guest-el');
+
+
+
+let homeScore = 12
+let guestScore = 20
+
+
+
+
+
+// fist score functions 
+
+function home1() {
+   homeScore = 13
+   homeEl.textContent = homeScore
+    
+}
+
+
+function home2() {
+    
+     homeScore = 14
+     homeEl.textContent = homeScore
+}
+
+function home3() {
+    homeScore = 15
+    homeEl.textContent = homeScore
+}
+
+
+
+
+function home4() {
+    homeScore = 12
+    homeEl.textContent = homeScore
+}
+
+function home5() {
+   homeScore = "1st"
+   homeEl.textContent = homeScore
+}
+
+function home6() {
+    homeScore = "H:6"
+    homeEl.textContent = homeScore
+}
+
+function home7() {
+    homeScore = "5:54"
+    homeEl.textContent = homeScore
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// the second score button functions
+
+
+function guest1() {
+    guestScore = 21
+    guestEl.textContent = guestScore
+    
+}
+
+
+function guest2() {
+    guestScore = 22
+    guestEl.textContent = guestScore
+}
+
+function guest3() {
+    guestScore = 23
+    guestEl.textContent = guestScore
+}
+
+
+
+
+
+
+function guest4() {
+    guestScore = 20
+    guestEl.textContent = guestScore
+}
+
+function guest5() {
+    guestScore = "2nd"
+    guestEl.textContent = guestScore
+}
+
+function guest6() {
+    guestScore = "G:1"
+    guestEl.textContent = guestScore
+}
+
+
+function guest7() {
+    guestScore = "1:00"
+    guestEl.textContent = guestScore
+    
+}
+
+
+
+
